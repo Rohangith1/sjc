098be1761010cbe0d7ff3246d0f2ef8696e9f358
Rohangith1 <107810397+Rohangith1@users.noreply.github.com>
2024-02-05 18:44:37 +0530

    connc estd

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,15 @@
 const connectToMongo = require('./db');
+const express= require('express')
 
 connectToMongo();
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.send("Helllo ")
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app Listening at http://localhost:${PORT}`)
+})
